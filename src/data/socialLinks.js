449c9163457694b1420b7ccb499b46d46d165d48
Dita <ditaputriutami29@gmail.com>
2026-04-06 21:16:20 +0700

    update link

---
+++ src/data/socialLinks.js
@@ -0,0 +1,29 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiMedium } from "react-icons/si";
+
+export const socialLinks = [
+  {
+    label: "GitHub",
+    href: "https://github.com/ditaputriutami",
+    Icon: FaGithub,
+    external: true,
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/dita-putri-utami29/",
+    Icon: FaLinkedin,
+    external: true,
+  },
+  {
+    label: "Email",
+    href: "ditaputriutami29@gmail.com",
+    Icon: FaEnvelope,
+    external: false,
+  },
+  {
+    label: "Medium",
+    href: "https://medium.com/@taditaputriutami",
+    Icon: SiMedium,
+    external: true,
+  },
+];
